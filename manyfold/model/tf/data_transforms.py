94ec4ca337dfd741fff1a83a23555e5a7909b2e4
amelvim <melyvimo@gmail.com>
2022-10-26 17:02:28 +0100

    Change headers

--- manyfold/model/tf/data_transforms.py
+++ manyfold/model/tf/data_transforms.py
@@ -1,10 +1,10 @@
-# Copyright 2021 DeepMind Technologies Limited
+# Copyright 2022 InstaDeep Ltd
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Creative Commons BY-NC-SA 4.0 License (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      https://creativecommons.org/licenses/by-nc-sa/4.0/
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Data for AlphaFold."""
+"""Data for AlphaFold/pLMFold."""
 from typing import Optional
 
 import numpy as np
